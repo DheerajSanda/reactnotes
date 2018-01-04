72ad1270a57a5edb65dd11ec8711c29cacb224d4
DheerajSanda <dheeraj.sanda@gmail.com>
2018-01-03 16:48:06 -0800

    first commit

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+
+  // Initialize Firebase
+export const DB_CONFIG = {
+    apiKey: "AIzaSyDLiN3tzm_jHhB-iDE-Oeo67es4oYe5HwY",
+    authDomain: "react-notes-8b444.firebaseapp.com",
+    databaseURL: "https://react-notes-8b444.firebaseio.com",
+    projectId: "react-notes-8b444",
+    storageBucket: "react-notes-8b444.appspot.com",
+    messagingSenderId: "495583149657"
+  };
